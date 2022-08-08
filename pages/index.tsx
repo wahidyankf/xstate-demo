4650b4bb08231057b00d5bbee504ddf376188115
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2022-08-09 06:55:58 +0700

    add simple machine link

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 const Home: NextPage = () => {
   return (
     <div>
+      <Link href="/traffic-light-01-simple">
+        Traffic Light Machine - 01 - Simple
+      </Link>
       <br></br>
       <Link href="/traffic-light-full">Traffic Light Machine - Full</Link>
-      <br></br>
     </div>
   );
 };
